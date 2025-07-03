fc6fa9c76cf18f75b35c4ba9c61026c0ebf6bdd5
jcpelera <jcpelera@gmail.com>
2025-07-03 17:37:46 -0600

    Finished repeating screen problem 6

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    if(num < 0){
+        return "ERROR";
+    }
+    let newStr = ""
+    for(let i = 0; i < num; i++){
+        newStr += word;
+    }
+    return newStr;
 };
 
 // Do not edit below this line

54971871215d83d5dc54a41b26561a17e6a43ab1
jcpelera <jcpelera@gmail.com>
2025-07-03 17:50:15 -0600

    Finished problem 7

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let reverseArr = word.split('');
+    let reverseWord = reverseArr.reverse();
+    reverseWord = reverseWord.join('');
+    return reverseWord;
 };
 
 // Do not edit below this line

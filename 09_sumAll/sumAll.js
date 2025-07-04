626440bcee70869a06995e8a99bf80aebc6cb08f
jcpelera <jcpelera@gmail.com>
2025-07-03 19:04:29 -0600

    Finished problem 9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,18 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    if(int1 < 0 || int2 < 0 || !Number.isInteger(int1)|| !Number.isInteger(int2)){
+        return "ERROR";
+    }
+    let sum = 0;
+    if(int1 < int2){
+        for(let i = int1; i <= int2; i++){
+            sum += i;
+        }
+        } else {
+            for(let i = int2; i <= int1; i++){
+            sum += i;
+        }
+    }
+    return sum;
 
 };
 

407eceb34561d7b58b92d985987f69ee95780c07
jcpelera <jcpelera@gmail.com>
2025-07-03 19:42:56 -0600

    Finished problem 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let celcius = (temp - 32) * (5/9);
+  if (celcius === 0){
+    return 0;
+  }
+  return parseFloat(celcius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let fahren = (temp *(9/5)) + 32;
+  if(fahren === 0){
+    return 0;
+  }
+  return parseFloat(fahren.toFixed(1));
 };
 
 // Do not edit below this line

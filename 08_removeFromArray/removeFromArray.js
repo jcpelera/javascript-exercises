83898db8d6fc573668344821f521c707edafc64f
jcpelera <jcpelera@gmail.com>
2025-07-03 18:20:22 -0600

    Partially done problem 8

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(list, target) {
+let targetIndex = list.indexOf(target);    
+if (target !== -1){
+let newArray = list.splice(targetIndex, 1);
+return newArray;
+}   
+return list;
 };
 
 // Do not edit below this line

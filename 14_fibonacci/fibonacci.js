1b84fd0e065ac0f30ad34abe8eb5812fc2de7707
jcpelera <jcpelera@gmail.com>
2025-07-09 22:10:17 -0600

    Finished fibonacci problem.

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let sequence;
+    if (num < 0){
+        return 'OOPS';
+    } else if (typeof num !== 'number'){
+        sequence = parseInt(num);
+    }
+    if (num === 0 || sequence === 0){
+        return 0;
+    }
+    let prev = 0;
+    let curr = 1;
+    let ahead;
 
+    for (let i = 0; i < num - 1; i++){
+        ahead = prev + curr;
+        prev = curr;
+        curr = ahead;
+    }
+    return curr;
 };
 
 // Do not edit below this line

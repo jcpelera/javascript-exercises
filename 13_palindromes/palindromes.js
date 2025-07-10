18b4665d7139288c79f08b065796be4b60a0f6e3
jcpelera <jcpelera@gmail.com>
2025-07-09 21:50:37 -0600

    Finished problem 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (phrase) {
+const cleanedString = phrase.replace(/[^\w\s]|_/g, '').replace(/\s/g, '').toLowerCase();
+const reversedString = cleanedString.split('').reverse().join('');
+if (cleanedString === reversedString){
+    return true;
+}else{
+    return false;
+}
 };
 
 // Do not edit below this line

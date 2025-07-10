7568cf3d281ff51d81e69815c7e2799531c7e1f9
jcpelera <jcpelera@gmail.com>
2025-07-09 21:36:26 -0600

    Completed practice 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((prev, curr) => prev + curr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+return arr.reduce((prev, curr) => prev * curr, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	let result = 1;
+  if (b === 0){
+    return 1;
+  }
+  for(let i = 0; i < b; i++){
+    result *= a;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if(a === 0){
+    return 1
+  }
+  return a * factorial(a-1);
 };
 
 // Do not edit below this line

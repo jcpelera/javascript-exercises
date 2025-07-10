57a26dd9a2d3beb4cac6e3636d417b7a2825b29b
jcpelera <jcpelera@gmail.com>
2025-07-09 22:38:53 -0600

    Finished all problems

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(name) {
+    const sortedPeople = name.toSorted((prev, curr) => getAge(curr)-getAge(prev));
+    const oldest = sortedPeople[0];
+    return oldest;
 };
+const getAge = function(name){
+    if(!name.yearOfDeath){
+        name.yearOfDeath = new Date().getFullYear();
+    }
+    return name.yearOfDeath - name.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
